1043aaf21cfe6a3ae52cf3d9c66a18660b789e1c
mworzala <mattheworzala@gmail.com>
2022-07-30 15:50:15 -0400

    add errorprone & nullaway

--- modules/build.gradle.kts
+++ modules/build.gradle.kts
@@ -1,6 +1,8 @@
+import net.ltgt.gradle.errorprone.errorprone
 
 subprojects {
     apply(plugin = "java")
+    apply(plugin = "net.ltgt.errorprone")
 
     repositories {
         mavenCentral()
@@ -13,6 +15,10 @@ subprojects {
         val implementation by configurations
         val annotationProcessor by configurations
         val testImplementation by configurations
+        val errorprone by configurations
+
+        errorprone("com.google.errorprone:error_prone_core:2.14.0")
+        errorprone("com.uber.nullaway:nullaway:0.9.8")
 
         // Auto service (SPI)
         annotationProcessor("com.google.auto.service:auto-service:1.0.1")
@@ -27,5 +33,13 @@ subprojects {
 
     tasks.getByName<Test>("test") {
         useJUnitPlatform()
+    }
+
+    tasks.withType<JavaCompile> {
+        options.errorprone.disableWarningsInGeneratedCode.set(true)
+        options.errorprone {
+            check("NullAway", net.ltgt.gradle.errorprone.CheckSeverity.ERROR)
+            option("NullAway:AnnotatedPackages", "com.uber")
+        }
     }
 }
