df6cc96d52961604a8c808ccb13711070e8dfd50
mworzala <mattheworzala@gmail.com>
2022-07-31 15:08:46 -0400

    enable online mode in dev server

--- modules/development/src/main/java/unnamed/mmo/server/dev/Main.java
+++ modules/development/src/main/java/unnamed/mmo/server/dev/Main.java
@@ -11,6 +11,7 @@ import net.minestom.server.entity.Player;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.PlayerLoginEvent;
 import net.minestom.server.event.player.PlayerSpawnEvent;
+import net.minestom.server.extras.MojangAuth;
 import net.minestom.server.instance.Instance;
 import net.minestom.server.instance.InstanceManager;
 import net.minestom.server.instance.block.Block;
@@ -25,6 +26,8 @@ public class Main {
 
     public static void main(String[] args) {
         MinecraftServer server = MinecraftServer.init();
+
+        MojangAuth.init();
 
         InstanceManager instanceManager = MinecraftServer.getInstanceManager();
 
