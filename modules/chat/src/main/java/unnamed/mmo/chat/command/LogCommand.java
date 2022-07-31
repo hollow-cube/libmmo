71ddf45e513e691b1a87e1de22ce2063a1b88594
mworzala <mattheworzala@gmail.com>
2022-07-30 23:07:46 -0400

    make some notes about date parsing

--- modules/chat/src/main/java/unnamed/mmo/chat/command/LogCommand.java
+++ modules/chat/src/main/java/unnamed/mmo/chat/command/LogCommand.java
@@ -21,6 +21,38 @@ public class LogCommand extends Command {
         super("log");
         this.storage = storage;
 
+        /*
+        Thinking about time filtering, there are a few things you might want to do
+        - All content on a specific day
+        /log 2022/12/10
+        - in the last 5 minutes
+        /log after now-5m
+        /log between now now-5m
+        - more than 5 minutes ago
+        /log before now-5m
+        - between 5 and 10 minutes ago
+        /log after now-10m before now-5m
+        - between day 1 and day 2
+        /log between 2022/12/2 2022/12/5
+        - in a specific year / month in a year
+        /log 2022/12
+        /log 2022
+        /log commands in 2022 on build_2 "glitch"
+
+        A "between" clause seems like it fixes a lot of these weird cases
+        Parsing all the variants here still seems like some effort
+        - date: 2022/12/2
+        - time: 10:23
+        - datetime: 2022/12/2-10:23 (dont love the -, but a space adds ambiguity)
+        - relative: now, now-2m, now-10d
+        /log between 2022/12 now
+        /log between now-12m now-2m
+        /log between 10:23 10:55
+
+        Having a parser for relative times, eg now-10m, 10m, etc
+        will be helpful in various places. Would much prefer to use an existing one though, but havent seen one yet.
+         */
+
         var filters = ArgumentType.Loop(
                 "filters",
                 ArgumentType.Group(
