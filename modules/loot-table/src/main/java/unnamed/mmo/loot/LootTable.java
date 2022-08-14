89fc0f9b0c2389f90e666398ff8f3d9ceb0e608d
mworzala <mattheworzala@gmail.com>
2022-08-14 14:39:57 +0300

    return LootResult from LootTable#generate

--- modules/loot-table/src/main/java/unnamed/mmo/loot/LootTable.java
+++ modules/loot-table/src/main/java/unnamed/mmo/loot/LootTable.java
@@ -4,6 +4,7 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minestom.server.utils.NamespaceID;
 import org.jetbrains.annotations.NotNull;
+import unnamed.mmo.loot.impl.LootResultImpl;
 import unnamed.mmo.registry.Resource;
 import unnamed.mmo.util.ExtraCodecs;
 
@@ -16,28 +17,6 @@ public record LootTable(
         @NotNull List<LootPool> pools
 ) implements Resource {
 
-    //Loot application Notes
-    // this is a little bit challenging, we want to apply the loot entries based on
-    // a registered handler for the given type, except in this case we want to
-    // override this to handle items explicitly.
-
-    // The item override is just to give the items some velocity on a block face
-    // this could be handled within the item drop handler, using some context
-    // information.
-
-    // Context in this case
-    // this: entity breaking the block
-    // pos: the position of the block
-    // direction: Direction to send rewards
-
-    // Final ask:
-    // - Apply based on handlers registered for a particular type. Two handlers for the same type is an error
-    // - Possible to override a handler to handle those loot entries explicitly.
-    // - LootResult is the new return type from generation of a table
-    //   - LootResult#override(Type.class, BiConsumer<Context, Type>)
-    //   - LootResult#apply(Context)
-    //   - LootResult#size()
-
     public static final Codec<LootTable> CODEC = RecordCodecBuilder.create(i -> i.group(
             ExtraCodecs.NAMESPACE_ID.fieldOf("namespace").forGetter(LootTable::namespace),
             LootModifier.CODEC.listOf().fieldOf("modifiers").forGetter(LootTable::modifiers),
@@ -49,12 +28,12 @@ public record LootTable(
         pools = List.copyOf(pools);
     }
 
-    public @NotNull List<@NotNull Object> generate(@NotNull LootContext context) {
-        return pools.stream()
+    public @NotNull LootResult generate(@NotNull LootContext context) {
+        return new LootResultImpl(pools.stream()
                 .map(pool -> pool.generate(context))
                 .flatMap(List::stream)
                 .map(this::applyModifiers)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
     }
 
     private @NotNull Object applyModifiers(@NotNull Object input) {
