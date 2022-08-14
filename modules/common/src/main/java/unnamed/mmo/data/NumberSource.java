30b045a62bb4cb9ced14dcc5388dc0fefb442918
mworzala <mattheworzala@gmail.com>
2022-08-14 23:49:34 +0300

    cleanup

--- modules/common/src/main/java/unnamed/mmo/data/NumberSource.java
+++ modules/common/src/main/java/unnamed/mmo/data/NumberSource.java
@@ -7,6 +7,7 @@ import java.util.concurrent.ThreadLocalRandom;
 /**
  * A source of numbers, implementations decide where the numbers come from
  */
+@FunctionalInterface
 public interface NumberSource {
 
     static @NotNull NumberSource constant(double value) {
