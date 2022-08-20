6a13d3d08c6718a98ce5eea84bca4750fb819ab1
GreatWyrm <alecmusante@gmail.com>
2022-08-19 19:26:09 -0700

    Add WeaponWeight enum and helper method for obtaining it

--- modules/player/src/main/java/unnamed/mmo/DamageTagList.java
+++ modules/player/src/main/java/unnamed/mmo/DamageTagList.java
@@ -1,12 +1,29 @@
 package unnamed.mmo;
 
+import net.minestom.server.item.ItemStack;
 import net.minestom.server.tag.Tag;
 
 public class DamageTagList {
 
     public static final Tag<Double> ENTITY_WEIGHT_TAG = Tag.Double("mob-weight");
+    public static final Tag<Integer> WEAPON_TYPE_TAG = Tag.Integer("weapon-weight");
 
-    public enum WeaponType {
+    public static WeaponWeight getWeaponWeight(ItemStack itemStack) {
+        if(itemStack.hasTag(WEAPON_TYPE_TAG)) {
+            int number = itemStack.getTag(WEAPON_TYPE_TAG);
+            WeaponWeight[] list = WeaponWeight.values();
+            if(number >= 1 && number < list.length) {
+                return list[number];
+            } else {
+                return WeaponWeight.NONE;
+            }
+        } else {
+            return WeaponWeight.NONE;
+        }
+    }
+
+    public enum WeaponWeight {
+        NONE,
         LIGHT,
         MEDIUM,
         HEAVY
