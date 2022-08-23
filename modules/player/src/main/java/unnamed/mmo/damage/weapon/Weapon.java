623f462c75b1f3d0defbdd5b8f5d213ec2c2041c
GreatWyrm <alecmusante@gmail.com>
2022-08-23 08:20:01 -0700

    Change apply in build files, and shift weapon weight over to Weapon record with Codec for it

--- modules/player/src/main/java/unnamed/mmo/damage/weapon/Weapon.java
+++ modules/player/src/main/java/unnamed/mmo/damage/weapon/Weapon.java
@@ -1,6 +1,9 @@
 package unnamed.mmo.damage.weapon;
 
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.DynamicOps;
+import com.mojang.serialization.codecs.PrimitiveCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import unnamed.mmo.item.ItemComponent;
 import unnamed.mmo.util.ExtraCodecs;
@@ -9,7 +12,8 @@ import java.util.List;
 
 public record Weapon(
         double attackSpeed,
-        List<AppliedPotionEffect> randomEffects
+        List<AppliedPotionEffect> randomEffects,
+        WeaponWeight weight
 
 ) implements ItemComponent {
 
@@ -19,9 +23,28 @@ public record Weapon(
             Codec.INT.fieldOf("tickDuration").forGetter(AppliedPotionEffect::tickDuration),
             Codec.INT.fieldOf("amplifier").forGetter(AppliedPotionEffect::amplifier)
     ).apply(i, AppliedPotionEffect::new));
+
+    private static final PrimitiveCodec<WeaponWeight> WEAPON_WEIGHT_CODEC = new PrimitiveCodec<>() {
+        @Override
+        public <T> DataResult<WeaponWeight> read(DynamicOps<T> ops, T input) {
+            DataResult<Number> number = ops.getNumberValue(input);
+            Number value = number.get().orThrow();
+            WeaponWeight weaponWeight = WeaponWeight.getWeight(value.intValue());
+            if(weaponWeight == null) {
+                return DataResult.error("Number was outside valid bounds!");
+            } else {
+                return DataResult.success(weaponWeight);
+            }
+        }
+        @Override
+        public <T> T write(DynamicOps<T> ops, WeaponWeight value) {
+            return ops.createNumeric(value.ordinal());
+        }
+    };
     public static final Codec<Weapon> CODEC = RecordCodecBuilder.create(i -> i.group(
             Codec.DOUBLE.fieldOf("attackSpeed").forGetter(Weapon::attackSpeed),
-            APPLIED_POTION_EFFECTS.listOf().optionalFieldOf("randomEffects", List.of()).forGetter(Weapon::randomEffects)
+            APPLIED_POTION_EFFECTS.listOf().optionalFieldOf("randomEffects", List.of()).forGetter(Weapon::randomEffects),
+            WEAPON_WEIGHT_CODEC.optionalFieldOf("weight", WeaponWeight.NONE).forGetter(Weapon::weight)
     ).apply(i, Weapon::new));
 
 }
