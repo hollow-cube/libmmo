1c16ff75fd8c91cc75adf22dcf7ac3e84db87d48
mworzala <mattheworzala@gmail.com>
2022-08-23 09:08:09 +0300

    copy safe dispatch from quest branch

--- modules/common/src/main/java/unnamed/mmo/util/EventUtil.java
+++ modules/common/src/main/java/unnamed/mmo/util/EventUtil.java
@@ -1,10 +1,13 @@
 package unnamed.mmo.util;
 
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.ServerProcess;
 import net.minestom.server.event.Event;
 import net.minestom.server.event.EventFilter;
 import net.minestom.server.event.EventNode;
 import net.minestom.server.event.trait.CancellableEvent;
 import org.jetbrains.annotations.NotNull;
+import unnamed.mmo.Env;
 
 import java.util.function.Predicate;
 
@@ -21,4 +24,23 @@ public class EventUtil {
         return event instanceof CancellableEvent c && c.isCancelled();
     }
 
+    /**
+     * Call an event in a test-safe manner.
+     */
+    public static void safeDispatch(@NotNull Event event) {
+        ServerProcess process = MinecraftServer.process();
+        if (process == null && !Env.STRICT_MODE)
+            return;
+        process.eventHandler().call(event);
+    }
+
+    /**
+     * Call an event in a test-safe manner.
+     */
+    public static void safeDispatchCancellable(@NotNull Event event, @NotNull Runnable onSuccess) {
+        ServerProcess process = MinecraftServer.process();
+        if (process == null && !Env.STRICT_MODE)
+            return;
+        process.eventHandler().callCancellable(event, onSuccess);
+    }
 }
