bba2a50c0d74ea03e58aef2cdef5197859d92f40
GreatWyrm <alecmusante@gmail.com>
2022-08-26 11:59:53 -0700

    Added probably somewhat janky support for shift clicking

--- modules/item/src/main/java/unnamed/mmo/item/crafting/CraftingInventory.java
+++ modules/item/src/main/java/unnamed/mmo/item/crafting/CraftingInventory.java
@@ -4,7 +4,11 @@ import net.kyori.adventure.text.Component;
 import net.minestom.server.entity.Player;
 import net.minestom.server.inventory.Inventory;
 import net.minestom.server.inventory.InventoryType;
+import net.minestom.server.inventory.PlayerInventory;
+import net.minestom.server.inventory.TransactionOption;
+import net.minestom.server.inventory.click.InventoryClickResult;
 import net.minestom.server.item.ItemStack;
+import net.minestom.server.utils.inventory.PlayerInventoryUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.TestOnly;
 
@@ -99,7 +103,75 @@ public class CraftingInventory extends Inventory {
 
     @Override
     public boolean shiftClick(@NotNull Player player, int slot) {
-        return super.shiftClick(player, slot);
+        // Shift click - will work as long as you can shift click it into an empty player slot
+        if (slot == 0 && !getItemStack(0).isAir()) {
+            int maxCrafts = 999;
+            for (int i = 1; i <= 9; i++) {
+                // TODO: This does not consider if more than one item is consumed in a craft
+                maxCrafts = Math.min(maxCrafts, getItemStack(i).amount());
+            }
+            final int maximumCrafts = maxCrafts;
+            // We have calculated the most number of items we can craft, see if we can insert into the inventory
+            int outputAmount = getItemStack(0).amount();
+            int totalItems = maxCrafts * outputAmount;
+            boolean result = player.getInventory().addItemStack(getItemStack(0).withAmount(totalItems), TransactionOption.DRY_RUN);
+            if(result) {
+                // We can add all items! go ahead and do so
+                player.getInventory().addItemStack(getItemStack(0).withAmount(totalItems), TransactionOption.ALL);
+                for (int i = 1; i <= 9; i++) {
+                    // TODO: This does not consider if more than one item is consumed in a craft
+                    if(getItemStack(i).amount() <= maximumCrafts) {
+                        setItemStack(i, ItemStack.AIR);
+                    } else {
+                        setItemStack(i, getItemStack(i).withAmount(integer -> integer - maximumCrafts));
+                    }
+                }
+            } else {
+                // Figure out how many we can add
+                for(int i = totalItems; i > 0; i -= outputAmount) {
+                    boolean canAdd = player.getInventory().addItemStack(getItemStack(0).withAmount(i), TransactionOption.DRY_RUN);
+                    if(canAdd) {
+                        player.getInventory().addItemStack(getItemStack(0).withAmount(totalItems), TransactionOption.ALL);
+                        final int craftNum = totalItems / outputAmount;
+                        for (int j = 1; j <= 9; j++) {
+                            // TODO: This does not consider if more than one item is consumed in a craft
+                            if(getItemStack(i).amount() <= craftNum) {
+                                setItemStack(i, ItemStack.AIR);
+                            } else {
+                                setItemStack(i, getItemStack(i).withAmount(integer -> integer - craftNum));
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+            updateCraftingRecipe();
+            return true;
+        }
+        // And then we copy the code from Inventory#shiftClick, except we start our clickProcessor search at 1 instead to avoid
+        // shift clicking things into the crafting slot
+        final PlayerInventory playerInventory = player.getInventory();
+        final boolean isInWindow = slot <= 9;
+        final int clickSlot = isInWindow ? slot : PlayerInventoryUtils.convertSlot(slot, 10);
+        final ItemStack clicked = isInWindow ? getItemStack(slot) : playerInventory.getItemStack(clickSlot);
+        final ItemStack cursor = getCursorItem(player); // Isn't used in the algorithm
+        final InventoryClickResult clickResult = this.clickProcessor.shiftClick(
+                isInWindow ? this : playerInventory,
+                isInWindow ? playerInventory : this,
+                1, isInWindow ? playerInventory.getInnerSize() : getInnerSize(), 1,
+                player, clickSlot, clicked, cursor);
+        if (clickResult.isCancel()) {
+            updateAll(player);
+            return false;
+        }
+        if (isInWindow) {
+            setItemStack(slot, clickResult.getClicked());
+        } else {
+            playerInventory.setItemStack(clickSlot, clickResult.getClicked());
+        }
+        updateAll(player); // FIXME: currently not properly client-predicted
+        setCursorItem(player, clickResult.getCursor());
+        return true;
     }
 
     private void updateCraftingRecipe() {
@@ -111,6 +183,11 @@ public class CraftingInventory extends Inventory {
             }
         }
         setItemStack(0, ItemStack.AIR);
+    }
+
+    private void updateAll(Player player) {
+        player.getInventory().update();
+        update(player);
     }
 
     @TestOnly
