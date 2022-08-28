3aba012e9eaeaa5155f5e429a2916764085299cc
mworzala <mattheworzala@gmail.com>
2022-08-28 17:50:00 +0300

    add slf4j wrapper, tinylog json lines writer with context support

--- modules/development/src/main/java/unnamed/mmo/server/dev/Main.java
+++ modules/development/src/main/java/unnamed/mmo/server/dev/Main.java
@@ -33,6 +33,8 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 
     public static void main(String[] args) {
+        System.setProperty("minestom.terminal.disabled", "true");
+
         MinecraftServer server = MinecraftServer.init();
 
         MojangAuth.init();
