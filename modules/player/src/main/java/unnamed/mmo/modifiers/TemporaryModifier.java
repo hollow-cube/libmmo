3317742fa569d5d56e856d89da253398bd21ee06
GreatWyrm <alecmusante@gmail.com>
2022-09-08 10:12:11 -0700

    Remove redundant methods

--- modules/player/src/main/java/unnamed/mmo/modifiers/TemporaryModifier.java
+++ modules/player/src/main/java/unnamed/mmo/modifiers/TemporaryModifier.java
@@ -3,16 +3,6 @@ package unnamed.mmo.modifiers;
 public record TemporaryModifier<T>(T modifierAmount, ModifierOperation operation, long expiryTimestamp) implements Modifier<T> {
 
     @Override
-    public T modifierAmount() {
-        return modifierAmount;
-    }
-
-    @Override
-    public ModifierOperation operation() {
-        return operation;
-    }
-
-    @Override
     public boolean hasExpired() {
         return System.currentTimeMillis() > expiryTimestamp;
     }
