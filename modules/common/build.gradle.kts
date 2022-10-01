2c4926776aa21bbdef8c3c549c9c082e07230128
Matt Worzala <35708499+mworzala@users.noreply.github.com>
2022-10-01 18:11:25 -0400

    Dev server improvements (#55)

--- modules/common/build.gradle.kts
+++ modules/common/build.gradle.kts
@@ -6,6 +6,9 @@ dependencies {
     api("com.github.hollow-cube:DataFixerUpper:cf58e926a6")
     api("net.kyori:adventure-text-minimessage:4.11.0")
 
+    api("com.github.mworzala.mc_debug_renderer:minestom:1.19.2-rv1")
+
+
     implementation("org.tinylog:tinylog-impl:2.4.1")
 
     implementation("io.github.cdimascio:dotenv-java:2.2.4")
